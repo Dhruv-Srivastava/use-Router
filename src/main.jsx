45d85fab61c86385690e6f8ffb12ad8bf7b2618e
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-06-28 04:51:14 +0530

    Demo of routing

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import BroswerRouter from "./lib/components/BrowserRouter.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BroswerRouter>
+      <App />
+    </BroswerRouter>
+  </React.StrictMode>
+);
