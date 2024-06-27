dc8e4c5028ba323b759e95c2e3e0d177ef8dc1f5
Dhruv-Srivastava <69876724+Dhruv-Srivastava@users.noreply.github.com>
2024-06-28 04:48:33 +0530

    Update BrowserRouter component to support forward and backward button of browser

--- src/lib/components/BrowserRouter.jsx
+++ src/lib/components/BrowserRouter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { BrowserContext } from "../context/BroswerContext";
 
@@ -7,7 +7,18 @@ export default function BroswerRouter({ children }) {
 
   // The purpose of this state is to store the current URL.
  //  We would require to know about path, url params, query strings, so instead of storing currentURL as a string we can store a URL object.
- 
+  
+  // Configure the foward and backward button of the broswer
+
+  useEffect(()=>{
+    function handlePopState(e){
+      console.log("history changed")
+      setCurrentURL(new URL(window.location.href))
+    }
+    window.addEventListener("popstate", handlePopState)
+
+    return () => window.removeEventListener("popstate", handlePopState)
+  }, [])
 
   const [currentURL, setCurrentURL] = useState(new URL(window.location.href));
   return (
